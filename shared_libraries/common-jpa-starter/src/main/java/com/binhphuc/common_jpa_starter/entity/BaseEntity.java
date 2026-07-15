14d10ad0b829ec4163b4de2c35e6352f3f879e67
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-07-16 01:17:48 +0700

    feat(shared-libraries): create common-jpa-starter for common use

--- shared_libraries/common-jpa-starter/src/main/java/com/binhphuc/common_jpa_starter/entity/BaseEntity.java
+++ shared_libraries/common-jpa-starter/src/main/java/com/binhphuc/common_jpa_starter/entity/BaseEntity.java
@@ -1,29 +1,25 @@
-package com.binhphuc.product_service.entity;
+package com.binhphuc.common_jpa_starter.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
-
 import java.time.Instant;
-
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
-  @Column(name = "is_deleted")
-  private Boolean isDeleted;
+  @Column(name = "is_deleted") private Boolean isDeleted;
 
   @CreatedDate
   @Column(name = "created_date", nullable = false, updatable = false)
