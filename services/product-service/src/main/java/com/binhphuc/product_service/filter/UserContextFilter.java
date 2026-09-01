ed776eb759356b7150668774fad16ddbfea15fa0
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-09-01 16:55:16 +0700

    fix(filter): add @Component annotation to bootstrap class

--- services/product-service/src/main/java/com/binhphuc/product_service/filter/UserContextFilter.java
+++ services/product-service/src/main/java/com/binhphuc/product_service/filter/UserContextFilter.java
@@ -8,11 +8,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.catalina.User;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class UserContextFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
