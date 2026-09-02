1cb1f2043b7d337d661c9c5f0743e39fb44c03f4
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-09-02 22:12:23 +0700

    feat(gateway): config redis rate limiter

--- gateway/src/main/java/com/binhphuc/api_gateway/config/RateLimiterConfig.java
+++ gateway/src/main/java/com/binhphuc/api_gateway/config/RateLimiterConfig.java
@@ -1,4 +1,18 @@
 package com.binhphuc.api_gateway.config;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import reactor.core.publisher.Mono;
+
+@Configuration
 public class RateLimiterConfig {
+    @Bean
+    public KeyResolver userIdKeyResolver() {
+        return exchange -> {
+            String userId = exchange.getPrincipal().cast(JwtAuthenticationToken.class).map(jwtAuth -> jwtAuth.getToken().getSubject()).toString();
+            return Mono.just(userId);
+        };
+    }
 }
