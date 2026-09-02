76d86bc8943022f26798549e3dc568ef8d44374d
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-09-02 22:13:20 +0700

    fix(script): flash sale sql script

--- infrastructure/databases/init/08-flash-sales.sql
+++ infrastructure/databases/init/08-flash-sales.sql
@@ -2,11 +2,10 @@ CREATE DATABASE IF NOT EXISTS flash_sales DEFAULT CHARACTER SET utf8mb4 COLLATE 
 
 USE flash_sales;
 
-CREATE TABLE IF NOT EXISTS flash_sales (
+CREATE TABLE IF NOT EXISTS campaigns (
     id                  VARCHAR(36)  NOT NULL,
     name                VARCHAR(255),
     description         TEXT,
-    status              VARCHAR(255),
     started_at          TIMESTAMP    NOT NULL,
     ended_at            TIMESTAMP    NOT NULL,
     created_by          VARCHAR(36),
@@ -17,21 +16,20 @@ CREATE TABLE IF NOT EXISTS flash_sales (
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS flash_sale_items (
+CREATE TABLE IF NOT EXISTS campaign_items (
     id                  VARCHAR(36)  NOT NULL,
-    flash_sale_id       VARCHAR(36),
-    product_id          VARCHAR(36),                 -- -> product_service.products.id
-    variant_id          VARCHAR(36),                 -- -> product_service.product_variants.id
-    flash_price         DECIMAL(19,2),
+    campaign_id       VARCHAR(36),
+    product_id          VARCHAR(36),
+    variant_id          VARCHAR(36),
+    price         DECIMAL(19,2),
     stock               BIGINT       NOT NULL DEFAULT 0,
     sold_quantity       BIGINT       NOT NULL DEFAULT 0,
-    purchase_limit      INT          NOT NULL DEFAULT 1,
     created_by          VARCHAR(36),
     created_at          TIMESTAMP    NOT NULL,
     last_modified_by    VARCHAR(36),
     last_modified_at    TIMESTAMP    NOT NULL,
     is_deleted          BOOLEAN      NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id),
-    UNIQUE KEY uk_flash_sale_items_variant (flash_sale_id, variant_id),
-    CONSTRAINT fk_flash_sale_items_flash_sale_id FOREIGN KEY (flash_sale_id) REFERENCES flash_sales (id)
+    UNIQUE KEY uk_flash_sale_items_variant (campaign_id, variant_id),
+    CONSTRAINT fk_flash_sale_items_campaign_id FOREIGN KEY (campaign_id) REFERENCES campaigns (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
